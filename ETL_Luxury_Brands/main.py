376d161e54b65859bdd854821229d03deaf585d5
Nikita Kent <nikitakent34@gmail.com>
2024-08-15 17:48:32 +0900

    X.

--- ETL_Luxury_Brands/main.py
+++ ETL_Luxury_Brands/main.py
@@ -5,5 +5,11 @@ import tweepy
 import os
 
 bearer_token = os.getenv("BEARER_TOKEN")
-
 client = tweepy.Client(bearer_token=bearer_token)
+
+## Cannot really access historical data, so we will just have to generate frequent requests to generate a backlog of data.
+# Measure: LVMH, Kering, Richemont, Michael Kors, Coach, 
+## Measures of brand popularity:
+## General attention (Number of posts)
+## Sentiment (Percentage of Negative posts vs Positive posts)
+## Engagement (Number of likes, retweets, comments)
